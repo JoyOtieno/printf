4a56376642d71e4dcacf49f74ebafc009bcc7a48
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-31 05:53:27 -0700

    print_str

--- 5-print_string.c
+++ 5-print_string.c
@@ -16,47 +16,45 @@
  */
 
 int print_string(va_list nums, char buffer[],
-int precision, int flags, int width, int size)
+		int precision, int flags, int width, int size)
 {
-	char *string, int padding, length = 0, printed_characters = 0;
+	char *string;
+	int length = 0, printed_characters = 0;
 
 	UNUSED(buffer), UNUSED(flags), UNUSED(size);
-	string = va_arg(nums, char *);
+	UNUSED(precision), UNUSED(width);
+	
 	if (string == NULL)
+	{
 		string = "(null)";
+		if (precision >= MIN_PRECISION)
+			string = " ";
+	}
 	while (string[length] != '\0')
 		length++;
+
 	if (precision >= 0 && precision < length)
 		length = precision;
+
 	if (width > length)
 	{
-		padding = width - length;
 		if (flags & F_MINUS)
 		{
-			write(1, string, length);
-			while (padding > 0)
-			{
-				write(1, "\t", 1);
-				padding--;
-				printed_characters++;
-			}
+			write(1, &str[0], length);
+			for (printed_characters = width - length; printed_
+					characters > 0; printed_characters--)
+				write(1, " ", 1);
+			return (width);
 		}
 		else
 		{
-			while (padding > 0)
-			{
-				write(1, "\t", 1);
-				padding--;
-				printed_characters++;
-			}
-			write(1, string, length);
+			for (printed_characters = width - length; printed_
+					characters > 0; printed_characters--)
+				write(1, " ", 1);
+			write(1, &string[0], length);
+			return (width);
 		}
-		printed_characters += length;
 	}
-	else
-	{
-		write(1, string, length);
-		printed_characters = length;
-	}
-	return (printed_characters);
+	return (write(1, string, length));
 }
+
